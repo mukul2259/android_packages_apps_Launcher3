d269a89dbc088c25d52334275129bb27686078fb
Anay Wadhera <awadhera@berkeley.edu>
2021-03-30 14:38:48 -0300

    Launcher3: Google feed integration
    Change-Id: I4dd635369513e7e8db8f8e4363acb017e0ce8eb1
    [kdrag0n: Debranded to com.android.launcher3]
    [kdrag0n: Ported to Android 12 and converted libGoogleFeed to Soong]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- quickstep/src/com/p404/launcher/P404Launcher.java
+++ quickstep/src/com/p404/launcher/P404Launcher.java
@@ -17,7 +17,13 @@
 package com.p404.launcher;
 
 import com.android.launcher3.uioverrides.QuickstepLauncher;
+import com.android.systemui.plugins.shared.LauncherOverlayManager;
 
 public class P404Launcher extends QuickstepLauncher {
+
+    @Override
+    protected LauncherOverlayManager getDefaultOverlay() {
+        return new OverlayCallbackImpl(this);
+    }
 
 }
